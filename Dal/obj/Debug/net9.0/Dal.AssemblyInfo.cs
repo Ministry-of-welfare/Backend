68cba47389d95d9ba52475dff35536a22dceb813
rivkypaluch <rivkypaluch@gmail.com>
2025-09-08 14:30:11 +0300

    getting changes from origin main

--- Dal/obj/Debug/net9.0/Dal.AssemblyInfo.cs
+++ Dal/obj/Debug/net9.0/Dal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eda61d3bdccb441ce97f7de276228c21cd10875")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be24f2e594eb6c6a08fd8bc7d26d2b8420854370")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
