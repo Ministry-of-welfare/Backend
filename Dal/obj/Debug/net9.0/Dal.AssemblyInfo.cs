e3fc393090bfb06796a8dfb43bc87ed3eef2d119
rivkypaluch <rivkypaluch@gmail.com>
2025-09-09 12:11:39 +0300

    ignore build artifacts

--- Dal/obj/Debug/net9.0/Dal.AssemblyInfo.cs
+++ Dal/obj/Debug/net9.0/Dal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68cba47389d95d9ba52475dff35536a22dceb813")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b317bd05b2eace7e8f54270999afd1df15efe3cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
