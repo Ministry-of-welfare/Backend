2e00fe3336b33f6ddb848a5a6491de896c30a867
rivkypaluch <rivkypaluch@gmail.com>
2025-10-19 15:41:04 +0300

    fixed

--- Dal/Models/AppDbContext.cs
+++ Dal/Models/AppDbContext.cs
@@ -54,9 +54,12 @@ public partial class AppDbContext : DbContext
     public virtual DbSet<TemplateStatus> TemplateStatuses { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-OUJMNEH;Initial Catalog=Practicum;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;");
-
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("name=DefaultConnection");
+        }
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<AppImportControl>(entity =>
