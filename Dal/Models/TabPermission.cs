ecd24d2afa844f0631a072961a7007e83567f931
chanikatz <chanikatz16@gmail.com>
2025-11-11 16:14:08 +0200

    מודלים להרשאות

---
+++ Dal/Models/TabPermission.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace server_pra.Dal.Models.ScaffoldEntities;
+
+/// <summary>
+/// רשימת ההרשאות במערכת (כגון צפייה, עריכה, אישור)
+/// </summary>
+public partial class TabPermission
+{
+    /// <summary>
+    /// מזהה ייחודי להרשאה
+    /// </summary>
+    public int PermissionId { get; set; }
+
+    /// <summary>
+    /// שם ההרשאה (כגון ViewFiles, ApproveImport)
+    /// </summary>
+    public string PermissionName { get; set; }
+
+    /// <summary>
+    /// תיאור קצר של ההרשאה
+    /// </summary>
+    public string PermissionDesc { get; set; }
+
+    /// <summary>
+    /// שם המודול במערכת שבו ההרשאה רלוונטית
+    /// </summary>
+    public string ModuleName { get; set; }
+
+    public virtual ICollection<TabRolePermission> TabRolePermissions { get; set; } = new List<TabRolePermission>();
+}
